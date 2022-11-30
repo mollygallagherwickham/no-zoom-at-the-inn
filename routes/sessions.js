0cdf2d6bd7b1b77f63ae64f4bd4f4ad80b4072ab
Molly Gallagher Wickham <mollyg210@gmail.com>
2022-11-30 15:10:53 +0000

    sessions setup

---
+++ routes/sessions.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+// connects to the controller
+const SessionsController = require('../controllers/sessions')
+
+router.get('/new', SessionsController.New)
+router.post('/', SessionsController.Create)
+router.delete('/', SessionsController.Destroy)
+
+module.exports = router
