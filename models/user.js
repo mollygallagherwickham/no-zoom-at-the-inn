8a51f0d970091f1b74f1371afd1861333f682284
PhilipStapel <113523770+PhilipStapel@users.noreply.github.com>
2022-12-01 12:33:30 +0000

    hopefullyfixederrors

--- models/user.js
+++ models/user.js
@@ -4,13 +4,7 @@ const UserSchema = new mongoose.Schema({
   first_name: String,
   last_name: String,
   email: String,
-<<<<<<< HEAD
-  last_name: String,
-  password: String,
-  first_name: String
-=======
   password: String
->>>>>>> main
 })
 
 const User = mongoose.model('User', UserSchema)
